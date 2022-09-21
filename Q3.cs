71e8d9a609d01437f088d3697010c81784e192e7
Saksham Gupta <sakshamgupta1973@gmail.com>
2022-09-21 09:56:53 +0530

    forked exp

--- Q3.cs
+++ Q3.cs
@@ -1,29 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CollectionsAssignment
-{
-    internal class Q3
-    {
-        public static void Main(string[] args)
-        {
-            SortedDictionary<int,string> arr = new SortedDictionary<int, string>();
+//namespace CollectionsAssignment
+//{
+//    internal class Q3
+//    {
+//        public static void Main(string[] args)
+//        {
+//            SortedDictionary<int,string> arr = new SortedDictionary<int, string>();
 
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Enter employee ID and name");
-                int empId = int.Parse(Console.ReadLine());
-                string empName = Console.ReadLine();
-                arr.Add(empId, empName);
-            }
+//            for (int i = 0; i < 5; i++)
+//            {
+//                Console.WriteLine("Enter employee ID and name");
+//                int empId = int.Parse(Console.ReadLine());
+//                string empName = Console.ReadLine();
+//                arr.Add(empId, empName);
+//            }
             
-            Console.WriteLine("Employee List :");
-            foreach (int i in arr.Keys) { 
-                Console.WriteLine(i+" "+arr[i]);
-            }
-        }
-    }
-}
+//            Console.WriteLine("Employee List :");
+//            foreach (int i in arr.Keys) { 
+//                Console.WriteLine(i+" "+arr[i]);
+//            }
+//        }
+//    }
+//}
+made few changed
